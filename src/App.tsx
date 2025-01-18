d855bd6c5ffe405e9f6e8558a17255608a52f061
arunalub <it22515612@my.sliit.lk>
2025-01-18 18:56:33 +0530

    Initialize Vite + React + TypeScript project with Tailwind CSS setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+
+  return (
+    <MantineProvider> 
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+       </h1>
+    </MantineProvider>
+  )
+}
+
+export default App
