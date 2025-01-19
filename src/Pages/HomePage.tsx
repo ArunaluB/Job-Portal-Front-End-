7b0155354d22c1799fef28394434830e3d612430
arunalub <it22515612@my.sliit.lk>
2025-01-19 12:52:19 +0530

    Browse Job page landing complted

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
 import Header from '../Header/Header'
 import DreamJob from '../LandingPage/DreamJob'
+import Companies from '../LandingPage/Companies'
+import JobCategory from '../LandingPage/JobCategory'
 
 const HomePage = () => {
   return (
     <div className="min-h-[100vh] bg-mine-shaft-950 font-['Poppins']">
       <Header />
       <DreamJob />
+      <Companies />
+      <JobCategory />
     </div>
 
   )
